cd5934ffca9fa6bc7f6a818a8b5b28e8501f7c11
ma08lane <ma08lane@siena.edu>
2017-04-20 13:10:57 -0400

    Add files via upload

--- CITY.java
+++ CITY.java
@@ -4,46 +4,50 @@
  * you hover over the city it will display the name. this is functionaility that
  * will need to be implemented later. there may be other uses for this enum.
  * 
- * @author (your name here)
- * @version (version number or date here)
+ * @author Leo Dohmann  
+ * @author Luke McKenna
+ * @author Emily DeLorenzo
+ * @author Matt LaNeve
+ * @author Edwin Svraka
+ * @version 4/19/17
  */
 public enum CITY
 {   
-    ONTARIO(94,64),
-    BUFFALO(396,65),
-    ERIE(171,198),
-    YOUNGSTOWN(65,390),
-    OILCITY(230,355),
-    WARREN(318,261),
-    DUBOIS(386,419),
-    ALTOONA(447,535),
-    PITTSBURGH(177,560),
-    WHEELING(77,638),
-    MORGANTOWN(193,745),
-    JOHNSTOWN(382,582),
-    CUMBERLAND(450,740),
-    ROCHESTER(643,67),
-    SYRACUSE(851,69),
-    WILLIAMSPORT(669,390),
-    TOWANDA(768,281),
-    COUDERSPORT(512,264),
-    LEWISTON(587,521),
-    CHAMBERSBURG(569,663),
-    GETTYSBURG(649,691),
-    BALTIMORE(808,741),
-    YORK(729,663),
-    LANCASTER(800,641),
-    ALLENTOWN(926,521),
-    PHILADELPHIA(1004,671),
-    ATLANTICCITY(1140,684),
-    ALBANY(1143,96),
-    BINGHAMTON(877,198),
-    ELMIRA(700,201),
-    SCRANTONWILKESBARRE(906,358),
-    NEWYORK(1141,395),
-    STROUDSBURG(1015,415),
-    READING(819,571),
-    HARRISBURG(694,584),
+    ONTARIO(69,48),
+    BUFFALO(298,50),
+    ERIE(128,148),
+    YOUNGSTOWN(51,293),
+    OILCITY(172,265),
+    WARREN(239,195),
+    DUBOIS(290,315),
+    ALTOONA(335,401),
+    PITTSBURGH(132,420),
+    WHEELING(57,478),
+    MORGANTOWN(144,560),
+    JOHNSTOWN(285,436),
+    CUMBERLAND(337,555),
+    ROCHESTER(484,50),
+    SYRACUSE(638,50),
+    WILLIAMSPORT(502,292),
+    TOWANDA(577,210),
+    COUDERSPORT(385,197),
+    LEWISTON(438,391),
+    CHAMBERSBURG(423,498),
+    GETTYSBURG(483,520),
+    BALTIMORE(604,557),
+    YORK(544,496),
+    LANCASTER(599,487),
+    ALLENTOWN(695,390),
+    PHILADELPHIA(754,502),
+    ATLANTICCITY(853,514),
+    ALBANY(857,74),
+    BINGHAMTON(656,148),
+    ELMIRA(523,151),
+    SCRANTONWILKESBARRE(679,268),
+    NEWYORK(857,297),
+    STROUDSBURG(757,331),
+    READING(615,425),
+    HARRISBURG(522,439),
     ;
     
     public int xCord;
