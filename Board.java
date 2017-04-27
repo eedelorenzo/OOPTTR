b9564a3cbbe182075ece9d0fe458fe048d0909a8
eedelorenzo <eedelorenzo@gmail.com>
2017-04-27 18:06:27 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -16,13 +16,17 @@ import java.util.Collections;
  * we will also most likely calculate how many of each card each player has in here, rather than 
  * outside of this class
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Leo Dohmann  
+ * @author Luke McKenna
+ * @author Emily DeLorenzo
+ * @author Matt LaNeve
+ * @author Edwin Svraka
+ * @version 5/1/17
  */
 public class Board
 {
     public ArrayList<TrainPath> listOfPaths;
-    ArrayList<STOCK> stockSet0;
+    //create arrayLists to hold the stocks for each path
     ArrayList<STOCK> stockSet1;
     ArrayList<STOCK> stockSet2;
     ArrayList<STOCK> stockSet3;
@@ -34,6 +38,59 @@ public class Board
     ArrayList<STOCK> stockSet9;
     ArrayList<STOCK> stockSet10;
     ArrayList<STOCK> stockSet11;
+    ArrayList<STOCK> stockSet12;
+    ArrayList<STOCK> stockSet13;
+    ArrayList<STOCK> stockSet14;
+    ArrayList<STOCK> stockSet15;
+    ArrayList<STOCK> stockSet16;
+    ArrayList<STOCK> stockSet17;
+    ArrayList<STOCK> stockSet18;
+    ArrayList<STOCK> stockSet19;
+    ArrayList<STOCK> stockSet20;
+    ArrayList<STOCK> stockSet21;
+    ArrayList<STOCK> stockSet22;
+    ArrayList<STOCK> stockSet23;
+    ArrayList<STOCK> stockSet24;
+    ArrayList<STOCK> stockSet25;
+    ArrayList<STOCK> stockSet26;
+    ArrayList<STOCK> stockSet27;
+    ArrayList<STOCK> stockSet28;
+    ArrayList<STOCK> stockSet29;
+    ArrayList<STOCK> stockSet30;
+    ArrayList<STOCK> stockSet31;
+    ArrayList<STOCK> stockSet32;
+    ArrayList<STOCK> stockSet33;
+    ArrayList<STOCK> stockSet34;
+    ArrayList<STOCK> stockSet35;
+    ArrayList<STOCK> stockSet36;
+    ArrayList<STOCK> stockSet37;
+    ArrayList<STOCK> stockSet38;
+    ArrayList<STOCK> stockSet39;
+    ArrayList<STOCK> stockSet40;
+    ArrayList<STOCK> stockSet41;
+    ArrayList<STOCK> stockSet42;
+    ArrayList<STOCK> stockSet43;
+    ArrayList<STOCK> stockSet44;
+    ArrayList<STOCK> stockSet45;
+    ArrayList<STOCK> stockSet46;
+    ArrayList<STOCK> stockSet47;
+    ArrayList<STOCK> stockSet48;
+    ArrayList<STOCK> stockSet49;
+    ArrayList<STOCK> stockSet50;
+    ArrayList<STOCK> stockSet51;
+    ArrayList<STOCK> stockSet52;
+    ArrayList<STOCK> stockSet53;
+    ArrayList<STOCK> stockSet54;
+    ArrayList<STOCK> stockSet55;
+    ArrayList<STOCK> stockSet56;
+    ArrayList<STOCK> stockSet57;
+    ArrayList<STOCK> stockSet58;
+    ArrayList<STOCK> stockSet59;
+    ArrayList<STOCK> stockSet60;
+    ArrayList<STOCK> stockSet61;
+    ArrayList<STOCK> stockSet62;
+    ArrayList<STOCK> stockSet63
+    
     /**
      * Constructor for objects of class Board
      */
@@ -147,49 +204,273 @@ public class Board
 
     public void initializeStockArrays(){
         //each railroad can have a different set of possible stocks to choose from
-        //my initial idea was to just create every possible set that exists 
-        //on the board and just assign them after
-        //as you can see, ive made an example of the first set using the path from ontario to erie
-        //if anyone has a better way to do this, go ahead, otherwise, we will need several
-        //sets of stocks as most routes use different amounts of each
+        //create every possible set that exists on the board
+        
+        //Ontario-Erie
         stockSet1.add(STOCK.PRR);
         stockSet1.add(STOCK.ERIE);
         stockSet1.add(STOCK.NYCENTRAL);
         
+        //Erie-Youngstown
         stockSet2.add(STOCK.ERIE);
         stockSet2.add(STOCK.NYCENTRAL);
         
+        //Buffalo-Rochester
         stockSet3.add(STOCK.ERIE);
         stockSet3.add(STOCK.NYCENTRAL);
         stockSet3.add(STOCK.BANDO);
         stockSet3.add(STOCK.BRANDP);
         stockSet3.add(STOCK.LEHIGH);
         
+        //Erie-Buffalo
         stockSet4.add(STOCK.ERIE);
         stockSet4.add(STOCK.NYCENTRAL);
         
+        //Buffalo-Coudersport
         stockSet5.add(STOCK.PRR);
         stockSet5.add(STOCK.ERIE);
         
+        //Warren-Buffalo
         stockSet6.add(STOCK.ERIE);
         stockSet6.add(STOCK.PRR);
         stockSet6.add(STOCK.BANDO);
         stockSet6.add(STOCK.BRANDP);
         
+        //Youngstown-Pittsburg
         stockSet7.add(STOCK.PRR);
         stockSet7.add(STOCK.BANDO);
         stockSet7.add(STOCK.NYCENTRAL);
         
+        //Youngstown-Oil City
         stockSet8.add(STOCK.ERIE);
         
+        //Oil City -Warren
         stockSet9.add(STOCK.PRR);
         stockSet9.add(STOCK.ERIE);
         stockSet9.add(STOCK.BANDO);
         
+        //Pittsburg-Oil City
         stockSet10.add(STOCK.PRR);
         stockSet10.add(STOCK.BANDO);
         
+        //Wheeling-Pittsburg
         stockSet11.add(STOCK.PRR);
+        
+        //Youngstown-Wheeling
+        stockSet12.add(STOCK.PRR);
+        stockSet12.add(STOCK.BANDO);
+        stockSet12.add(STOCK.NYCENTRAL);
+        
+        //Wheeling-Morgantown
+        stockSet13.add(STOCK.BANDO);
+        
+        //Erie-Warren
+        stockSet14.add(STOCK.PRR);
+        stockSet14.add(STOCK.ERIE);
+        stockSet14.add(STOCK.NYCENTRAL);
+        
+        //Erie-Oil City
+        stockSet15.add(STOCK.PRR);
+        stockSet15.add(STOCK.ERIE);
+        stockSet15.add(STOCK.NYCENTRAL);
+        
+        //Pittsburg-Johnstown
+        stockSet16.add(STOCK.PRR);
+        stockSet16.add(STOCK.BANDO);
+        
+        //Morgantown-Cumberland
+        stockSet17.add(STOCK.WM);
+        stockSet17.add(STOCK.BANDO);
+        
+        //Warren-Coudersport
+        stockSet18.add(STOCK.PRR);
+        stockSet18.add(STOCK.ERIE);
+        stockSet18.add(STOCK.BANDO);
+        stockSet18.add(STOCK.NYCENTRAL);
+        stockSet18.add(STOCK.BRANDP);
+        
+        //Warren-Dubois
+        stockSet19.add(STOCK.ERIE);
+        stockSet19.add(STOCK.BRANDP);
+        
+        //Dubois-Altoona
+        stockSet20.add(STOCK.PRR);
+        
+        //Johnstown-Altoona
+        stockSet21.add(STOCK.PRR);
+        
+        //Johnstown-Cumberland
+        stockSet22.add(STOCK.WM);
+        stockSet22.add(STOCK.PRR);
+        stockSet22.add(STOCK.BANDO);
+        
+        //Coudersport-Elmira
+        stockSet23.add(STOCK.PRR);
+        stockSet23.add(STOCK.ERIE);
+        stockSet23.add(STOCK.BANDO);
+        stockSet23.add(STOCK.NYCENTRAL);
+        stockSet23.add(STOCK.BRANDP);
+        
+        //Coudersport-Williamsport
+        stockSet24.add(STOCK.PRR);
+        
+        //Altoona-Harrisburg
+        stockSet25.add(STOCK.PRR);
+        
+        //Cumberland-Chambersburg
+        stockSet26.add(STOCK.WM);
+        stockSet26.add(STOCK.PRR);
+        
+        //Rochester-Elmira
+        stockSet27.add(STOCK.PRR);
+        stockSet27.add(STOCK.ERIE);
+        stockSet27.add(STOCK.BANDO);
+        stockSet27.add(STOCK.LEHIGH);
+        stockSet27.add(STOCK.NYCENTRAL);
+        stockSet27.add(STOCK.BRANDP);
+        
+        //Rochester-Syracuse
+        stockSet28.add(STOCK.NYCENTRAL);
+        stockSet28.add(STOCK.LEHIGH);
+        
+        //Lewiston-Williamsport
+        stockSet29.add(STOCK.PRR);
+        stockSet29.add(STOCK.READINGLINE);
+        
+        //Lewiston-Harrisburg
+        stockSet30.add(STOCK.PRR);
+        
+        //Chambersburg-Harrisburg
+        stockSet31.add(STOCK.WM);
+        stockSet31.add(STOCK.PRR);
+        stockSet31.add(STOCK.READINGLINE);
+        
+        //Cumberland-Baltimore
+        stockSet32.add(STOCK.WM);
+        stockSet32.add(STOCK.BANDO);
+        
+        //Elmira-Syracuse
+        stockSet33.add(STOCK.ERIE);
+        stockSet33.add(STOCK.LEHIGH);
+        
+        //Elmira-Binghamton
+        stockSet34.add(STOCK.ERIE);
+        
+        //Elmira-Towanda
+        stockSet35.add(STOCK.PRR);
+        stockSet35.add(STOCK.ERIE);
+        stockSet35.add(STOCK.LEHIGH);
+        
+        //Willamsport-Towanda
+        stockSet36.add(STOCK.ERIE);
+        stockSet36.add(STOCK.READINGLINE);
+        
+        //Gettysburg-Baltimore
+        stockSet37.add(STOCK.WM);
+        stockSet37.add(STOCK.PRR);
+        
+        //Gettysburg-York
+        stockSet38.add(STOCK.WM);
+        stockSet38.add(STOCK.PRR);
+        
+        //Gettysburg-Harrisburg
+        stockSet39.add(STOCK.PRR);
+        stockSet39.add(STOCK.READINGLINE);
+        
+        //Harrisburg-York
+        stockSet40.add(STOCK.WM);
+        stockSet40.add(STOCK.PRR);
+        
+        //Harrisburg-Scranton
+        stockSet41.add(STOCK.PRR);
+        stockSet41.add(STOCK.READINGLINE);
+        
+        //Harrisburg-Reading
+        stockSet42.add(STOCK.READINGLINE);
+        
+        //Harrisburg-Lancaster
+        stockSet43.add(STOCK.PRR);
+        
+        //York-Lancaster
+        stockSet44.add(STOCK.PRR);
+        
+        //York-Baltimore
+        stockSet45.add(STOCK.WM);
+        stockSet45.add(STOCK.PRR);
+        
+        //Towanda-Binghamton
+        stockSet46.add(STOCK.ERIE);
+        
+        //Towanda-Scranton
+        stockSet47.add(STOCK.PRR);
+        stockSet47.add(STOCK.ERIE);
+        stockSet47.add(STOCK.LEHIGH);
+        
+        //Reading-Allentown
+        stockSet48.add(STOCK.READINGLINE);
+        
+        //Reading-Lancaster
+        stockSet49.add(STOCK.READINGLINE);
+        
+        //Lancaster-Philadelphia
+        stockSet50.add(STOCK.PRR);
+        
+        //Baltimore-Philadelphia
+        stockSet51.add(STOCK.PRR);
+        stockSet51.add(STOCK.BANDO);
+        
+        //Syracuse-Albany
+        stockSet52.add(STOCK.NYCENTRAL);
+        
+        //Syracuse-Binghamton
+        stockSet53.add(STOCK.ERIE);
+        
+        //Binghamton-Scranton
+        stockSet54.add(STOCK.ERIE);
+        
+        //Scranton-New York
+        stockSet55.add(STOCK.ERIE);
+        stockSet55.add(STOCK.LEHIGH);
+        stockSet55.add(STOCK.JCL);
+        
+        //Scranton-Stroudsburg
+        stockSet56.add(STOCK.READINGLINE);
+        stockSet56.add(STOCK.LEHIGH);
+        stockSet56.add(STOCK.JCL);
+        
+        //Scranton-Allentown
+        stockSet57.add(STOCK.JCL);
+        stockSet57.add(STOCK.LEHIGH);
+        stockSet57.add(STOCK.READINGLINE);
+        stockSet57.add(STOCK.PRR);
+        
+        //Allentown-Stroudsburg
+        stockSet58.add(STOCK.READINGLINE);
+        stockSet58.add(STOCK.LEHIGH);
+        stockSet58.add(STOCK.JCL);
+        
+        //Allentown-Philadelphia
+        stockSet59.add(STOCK.PRR);
+        stockSet59.add(STOCK.READINGLINE);
+        
+        //Philadelphia-Atlantic City
+        stockSet60.add(STOCK.PRR);
+        stockSet60.add(STOCK.READINGLINE);
+        stockSet60.add(STOCK.JCL);
+        
+        //Philadelphia-New York
+        stockSet61.add(STOCK.PRR);
+        stockSet61.add(STOCK.BANDO);
+        stockSet61.add(STOCK.READINGLINE);
+        stockSet61.add(STOCK.JCL);
+        
+        //Albany-New York
+        stockSet62.add(STOCK.PRR);
+        stockSet62.add(STOCK.BANDO);
+        stockSet62.add(STOCK.NYCENTRAL);
+        
+        //New York-Atlantic City
+        stockSet63.add(STOCK.JCL);
     }
     
     public int[] genColorTotals(PlayerCharacter activePlayer){
